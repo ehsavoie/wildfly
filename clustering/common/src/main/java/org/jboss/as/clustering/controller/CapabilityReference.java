a88575f60e3a5fc6b61cb76c4275bf09b2649cea
Emmanuel Hugonnet <emmanuel.hugonnet+gitkraken@gmail.com>
2018-01-30 17:05:36 +0100

    Updating feature groups with origin for feature pack reference Having ha and full-ha configs almost correct.

--- clustering/common/src/main/java/org/jboss/as/clustering/controller/CapabilityReference.java
+++ clustering/common/src/main/java/org/jboss/as/clustering/controller/CapabilityReference.java
@@ -23,6 +23,7 @@
 package org.jboss.as.clustering.controller;
 
 import java.util.Optional;
+import java.util.StringJoiner;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.stream.Stream;
@@ -44,14 +45,15 @@ public class CapabilityReference implements CapabilityReferenceRecorder {
     private final Capability capability;
     private final Requirement requirement;
     private final BiFunction<OperationContext, String, Optional<String>> requirementResolver;
+    private final String[] dynamicParts;
 
     /**
      * Creates a new reference between the specified capability and the specified requirement
      * @param capability the capability referencing the specified requirement
      * @param requirement the requirement of the specified capability
      */
-    public CapabilityReference(Capability capability, UnaryRequirement requirement) {
-        this(capability, requirement, (context, value) -> (value != null) ? Optional.of(requirement.resolve(value)) : Optional.empty());
+    public CapabilityReference(Capability capability, UnaryRequirement requirement, String... dynamicParts) {
+        this(capability, requirement, (context, value) -> (value != null) ? Optional.of(requirement.resolve(value)) : Optional.empty(), dynamicParts);
     }
 
     /**
@@ -59,8 +61,8 @@ public class CapabilityReference implements CapabilityReferenceRecorder {
      * @param capability the capability referencing the specified requirement
      * @param requirement the requirement of the specified capability
      */
-    public CapabilityReference(Capability capability, BinaryRequirement requirement) {
-        this(capability, requirement, OperationContext::getCurrentAddressValue);
+    public CapabilityReference(Capability capability, BinaryRequirement requirement, String... dynamicParts) {
+        this(capability, requirement, OperationContext::getCurrentAddressValue, dynamicParts);
     }
 
     /**
@@ -69,28 +71,28 @@ public class CapabilityReference implements CapabilityReferenceRecorder {
      * @param requirement the requirement of the specified capability
      * @param parentAttribute the attribute containing the value of the parent dynamic component of the requirement
      */
-    public CapabilityReference(Capability capability, BinaryRequirement requirement, Attribute parentAttribute) {
-        this(capability, requirement, context -> context.readResource(PathAddress.EMPTY_ADDRESS, false).getModel().get(parentAttribute.getName()).asString());
+    public CapabilityReference(Capability capability, BinaryRequirement requirement, Attribute parentAttribute, String... dynamicParts) {
+        this(capability, requirement, context -> context.readResource(PathAddress.EMPTY_ADDRESS, false).getModel().get(parentAttribute.getName()).asString(), dynamicParts);
     }
-
     /**
      * Creates a new reference between the specified capability and the specified requirement
      * @param capability the capability referencing the specified requirement
      * @param requirement the requirement of the specified capability
      * @param parentResolver the resolver of the parent dynamic component of the requirement
      */
-    public CapabilityReference(Capability capability, BinaryRequirement requirement, Function<OperationContext, String> parentResolver) {
-        this(capability, requirement, (context, value) -> (value != null) ? Optional.of(requirement.resolve(parentResolver.apply(context), value)) : Optional.empty());
+    public CapabilityReference(Capability capability, BinaryRequirement requirement, Function<OperationContext, String> parentResolver, String... dynamicParts) {
+        this(capability, requirement, (context, value) -> (value != null) ? Optional.of(requirement.resolve(parentResolver.apply(context), value)) : Optional.empty(), dynamicParts);
     }
 
-    CapabilityReference(Capability capability, Requirement requirement, BiFunction<OperationContext, String, Optional<String>> requirementResolver) {
+    CapabilityReference(Capability capability, Requirement requirement, BiFunction<OperationContext, String, Optional<String>> requirementResolver, String... dynamicParts) {
         this.capability = capability;
         this.requirement = requirement;
         this.requirementResolver = requirementResolver;
+        this.dynamicParts = dynamicParts;
     }
 
     @Override
-    public void addCapabilityRequirements(OperationContext context, Resource resource,  String attributeName, String... values) {
+    public void addCapabilityRequirements(OperationContext context, Resource resource, String attributeName, String... values) {
         String dependentName = this.capability.resolve(context.getCurrentAddress()).getName();
         Stream.of(values).forEach(value -> this.requirementResolver.apply(context, value).ifPresent(requirementName -> context.registerAdditionalCapabilityRequirement(requirementName, dependentName, attributeName)));
     }
@@ -120,6 +122,23 @@ public class CapabilityReference implements CapabilityReferenceRecorder {
     @Override
     public int hashCode() {
         return this.capability.getDefinition().getName().hashCode();
+    }
+
+    @Override
+    public String getRequirementName(String... dynamicElements) {
+        StringJoiner pattern = new StringJoiner(".$");
+        pattern.add(requirement.getName());
+        if (dynamicParts != null && dynamicParts.length > 0) {
+            for (String part : dynamicParts) {
+                pattern.add(part);
+            }
+        }
+        if (dynamicElements != null && dynamicElements.length > 0) {
+            for (String part : dynamicElements) {
+                pattern.add(part);
+            }
+        }
+        return pattern.toString();
     }
 
     @Override
