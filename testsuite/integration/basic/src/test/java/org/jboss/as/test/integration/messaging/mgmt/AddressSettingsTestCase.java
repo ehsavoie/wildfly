4cbab9fe8d7367b74436538ff5cb95240bc65e61
Emmanuel Hugonnet <ehugonne@redhat.com>
2023-10-12 15:36:26 +0200

    [WFLY-18547]:  Trying to fix default values
    Signed-off-by: Emmanuel Hugonnet <ehugonne@redhat.com>

--- testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/messaging/mgmt/AddressSettingsTestCase.java
+++ testsuite/integration/basic/src/test/java/org/jboss/as/test/integration/messaging/mgmt/AddressSettingsTestCase.java
@@ -13,6 +13,7 @@ import static org.jboss.as.controller.descriptions.ModelDescriptionConstants.REA
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.io.IOException;
 import java.util.Set;
 
 import org.jboss.arquillian.container.test.api.RunAsClient;
@@ -23,6 +24,7 @@ import org.jboss.as.controller.descriptions.ModelDescriptionConstants;
 import org.jboss.as.test.integration.common.jms.JMSOperations;
 import org.jboss.as.test.integration.common.jms.JMSOperationsProvider;
 import org.jboss.as.test.integration.management.base.ContainerResourceMgmtTestBase;
+import org.jboss.as.test.integration.management.util.MgmtOperationException;
 import org.jboss.dmr.ModelNode;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,10 +39,9 @@ public class AddressSettingsTestCase extends ContainerResourceMgmtTestBase {
     private static final String ACTIVEMQ_ADDRESS = "activemq-address";
     private static final String MESSAGE_COUNTER_HISTORY_DAY_LIMIT = "message-counter-history-day-limit";
     private static final String RESOLVE_ADDRESS_SETTING = "resolve-address-setting";
-    private static final String MAX_READ_PAGE_BYTES = "max-read-page-bytes";
-    private static final String PAGE_SIZE_BYTES = "page-size-bytes";
 
     private ModelNode defaultAddress;
+    private ModelNode intermediateAddress;
     private ModelNode address;
 
     @ContainerResource
@@ -49,11 +50,22 @@ public class AddressSettingsTestCase extends ContainerResourceMgmtTestBase {
     private JMSOperations jmsOperations;
 
     @Before
-    public void before() {
+    public void before() throws IOException, MgmtOperationException {
         jmsOperations = JMSOperationsProvider.getInstance(managementClient);
 
         defaultAddress = jmsOperations.getServerAddress().add("address-setting", "#");
+        intermediateAddress = jmsOperations.getServerAddress().add("address-setting", "jms.queue.*");
         address = jmsOperations.getServerAddress().add("address-setting", "jms.queue.foo");
+        try {
+            remove(address);
+        } catch (MgmtOperationException ex) {
+            //ignore
+        }
+        try {
+            remove(intermediateAddress);
+        } catch (MgmtOperationException ex) {
+            //ignore
+        }
     }
 
     @Test
@@ -70,10 +82,7 @@ public class AddressSettingsTestCase extends ContainerResourceMgmtTestBase {
         update.get(ModelDescriptionConstants.OP_ADDR).set(address);
         update.get(ModelDescriptionConstants.NAME).set("redistribution-delay");
         update.get(ModelDescriptionConstants.VALUE).set(-1L);
-
         executeOperation(update);
-
-        remove(address);
     }
 
     @Test
@@ -99,8 +108,41 @@ public class AddressSettingsTestCase extends ContainerResourceMgmtTestBase {
         ModelNode result = executeOperation(resolve);
 
         for (String attributeName : attributeNames) {
-            if (attributeName.equals(MAX_READ_PAGE_BYTES)) {
-                assertEquals("unexpected value for " + attributeName, result.get(PAGE_SIZE_BYTES).asInt() * 2, result.get(attributeName).asInt());
+            assertEquals("unexpected value for " + attributeName, defaultAddressSetting.get(attributeName), result.get(attributeName));
+        }
+    }
+
+    @Test
+    public void testResolveMergedAddressSettings() throws Exception {
+        final ModelNode readResourceDescription = new ModelNode();
+        readResourceDescription.get(ModelDescriptionConstants.OP_ADDR).set(defaultAddress);
+        readResourceDescription.get(ModelDescriptionConstants.OP).set(READ_RESOURCE_DESCRIPTION_OPERATION);
+        final ModelNode description = executeOperation(readResourceDescription, true);
+        Set<String> attributeNames = description.get(ATTRIBUTES).keys();
+
+        final ModelNode readResource = new ModelNode();
+        readResource.get(ModelDescriptionConstants.OP_ADDR).set(defaultAddress);
+        readResource.get(ModelDescriptionConstants.OP).set(READ_RESOURCE_OPERATION);
+        final ModelNode defaultAddressSetting = executeOperation(readResource);
+
+        final ModelNode add = new ModelNode();
+        add.get(ModelDescriptionConstants.OP).set(ADD);
+        add.get(ModelDescriptionConstants.OP_ADDR).set(intermediateAddress);
+        add.get("page-size-bytes").set(1024*1024);
+        executeOperation(add);
+
+        // there is no address-setting for the given address but
+        // we can resolve its settings based on HornetQ hierarchical
+        // repository of address setting.
+        final ModelNode resolve = new ModelNode();
+        resolve.get(ModelDescriptionConstants.OP_ADDR).set(jmsOperations.getServerAddress());
+        resolve.get(ModelDescriptionConstants.OP).set(RESOLVE_ADDRESS_SETTING);
+        resolve.get(ACTIVEMQ_ADDRESS).set("jms.queue.foo");
+        ModelNode result = executeOperation(resolve);
+
+        for (String attributeName : attributeNames) {
+            if("page-size-bytes".equals(attributeName)) {
+                assertEquals("unexpected value for " + attributeName, 1024*1024, result.get(attributeName).asInt());
             } else {
                 assertEquals("unexpected value for " + attributeName, defaultAddressSetting.get(attributeName), result.get(attributeName));
             }
@@ -130,7 +172,5 @@ public class AddressSettingsTestCase extends ContainerResourceMgmtTestBase {
         result = executeOperation(resolve);
         // inherit the message-counter-history-day-limit for the '#' address-setting
         assertEquals(10, result.get(MESSAGE_COUNTER_HISTORY_DAY_LIMIT).asInt());
-
-        remove(address);
     }
 }
